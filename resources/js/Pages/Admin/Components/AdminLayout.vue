bf0d6f56a244c72f556325dbc48696d267cd4772
Markus <lebrabusiness@gmail.com>
2024-05-30 15:03:24 +0300

    all the table migrations for shop

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Navbar from './Navbar.vue'
+import Sidebar from './Sidebar.vue'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+
+</script>
+
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!-- nav bar -->
+        <Navbar/>
+        <!-- Sidebar -->
+
+        <Sidebar/>
+
+        <slot />
+    </div>
+</template>
