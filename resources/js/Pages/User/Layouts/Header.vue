15e5f558a05ac9f4566f4554638155964f09c39b
Markus <lebrabusiness@gmail.com>
2024-06-04 21:58:22 +0300

    cart system

--- resources/js/Pages/User/Layouts/Header.vue
+++ resources/js/Pages/User/Layouts/Header.vue
@@ -4,6 +4,7 @@ import { usePage, Link } from "@inertiajs/vue3";
 const canLogin = usePage().props.canLogin;
 const canRegister = usePage().props.canRegister;
 const auth = usePage().props.auth;
+
 </script>
 
 <template>
@@ -29,22 +30,27 @@ const auth = usePage().props.auth;
                 v-if="canLogin"
                 class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"
             >
-                <div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="size-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                        />
-                    </svg>
-                </div>
+            <div class="mr-4">
+
+<Link :href="route('dashboard')"
+    class="relative inline-flex items-center p-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 ">
+    <path stroke-linecap="round" stroke-linejoin="round"
+        d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+</svg>
+
+<span class="sr-only">cart</span>
+<div
+    class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+    h</div>
+</Link>
+
+
+</div>
+
+
+                
 
                 <button
                     v-if="auth.user"
@@ -94,10 +100,10 @@ const auth = usePage().props.auth;
                     </div>
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
-                            <a
-                                href="#"
+                            <Link
+                                :href="route('dashboard')"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >Dashboard</a
+                                >Dashboard</Link
                             >
                         </li>
                         <li>
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\CartController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-Route::get('/', [UserController::class, 'index'])->name('user');
+Route::get('/', [UserController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -25,6 +26,19 @@ Route::middleware('auth')->group(function () {
     // }));
   
 });
+
+Route::prefix('cart')->controller(CartController::class)->group(function () {
+    Route::get('view','view')->name('cart.view');
+    Route::post('store/{product}','store')->name('cart.store');
+    Route::patch('update/{product}','update')->name('cart.update');
+    Route::delete('delete/{product}','delete')->name('cart.delete');
+});
+
+//routes for products list and filter 
+// Route::prefix('products')->controller(ProductListController::class)->group(function ()  {
+//     Route::get('/','index')->name('products.index');
+    
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
