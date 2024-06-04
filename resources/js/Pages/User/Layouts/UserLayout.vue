a9c0fd2fbbf237e583c6afb566cd702d2ddefde6
Markus <lebrabusiness@gmail.com>
2024-06-04 20:48:51 +0300

    User view with products on index

---
+++ resources/js/Pages/User/Layouts/UserLayout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+onMounted(() => {
+    initFlowbite();
+});
+
+
+
+
+</script>
+
+<template>
+    <Header/>
+
+
+    <slot/>
+
+    <Footer/>
+
+    
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,13 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [UserController::class, 'index'])->name('user');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
